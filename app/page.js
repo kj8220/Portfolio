bab7ba5643a866d02c74215d089366210b2c6073
roopanraj_perumalraj <roopanraj.perumalraj@42-q.com>
2025-08-22 15:12:45 +0530

    Feat: commented education part

--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@
 import { personalData } from "@/utils/data/personal-data";
 //import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
+//import Education from "./components/homepage/education";
+//import Experience from "./components/homepage/experience";
 //import HeroSection from "./components/homepage/hero-section";
 //import Projects from "./components/homepage/projects";
 // import Skills from "./components/homepage/skills"; // <-- Keep this commented out
@@ -24,10 +24,10 @@ export default async function Home() {
     <div>
      {/* <HeroSection />*/}
       {/*<AboutSection />*/}
-      <Experience />
+      {/*<Experience />*/}
       {/* <Skills /> */} {/* <-- Keep this commented out */}
       {/* <Projects />*/}
-      <Education />
+     {/* <Education />*/}
       <ContactSection />
     </div>
   )
