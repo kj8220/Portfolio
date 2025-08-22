f6a2b9185d3ffb86f177ae89012a5d13cea6639c
roopanraj_perumalraj <roopanraj.perumalraj@42-q.com>
2025-08-22 15:19:30 +0530

    Feat: added experience part

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 //import Education from "./components/homepage/education";
-//import Experience from "./components/homepage/experience";
+import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 //import Projects from "./components/homepage/projects";
 // import Skills from "./components/homepage/skills"; // <-- Keep this commented out
@@ -24,7 +24,7 @@ export default async function Home() {
     <div>
       <HeroSection />
       <AboutSection />
-      {/*<Experience />*/}
+      <Experience />
       {/* <Skills /> */} {/* <-- Keep this commented out */}
       {/* <Projects />*/}
      {/* <Education />*/}
