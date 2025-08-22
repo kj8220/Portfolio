ee8e7ce6b514e46a2d918eb383c9f2af873240aa
roopanraj_perumalraj <roopanraj.perumalraj@42-q.com>
2025-08-22 12:54:49 +0530

    commented out the page js part

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
-import Skills from "./components/homepage/skills";
+//import Skills from "./components/homepage/skills";
 
 async function getData() {
   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
@@ -28,7 +28,7 @@ export default async function Home() {
       <HeroSection />
       <AboutSection />
       <Experience />
-      <Skills />
+      {/* <Skills /> */} {/* <-- AND COMMENT THIS LINE OUT */}
       <Projects />
       <Education />
       <ContactSection />
