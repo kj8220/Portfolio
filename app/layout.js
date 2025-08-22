688969a3c92437aa5852f1d4327cb2246be1be9b
roopanraj_perumalraj <roopanraj.perumalraj@42-q.com>
2025-08-22 14:03:40 +0530

    commented navbar

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import { GoogleTagManager } from "@next/third-parties/google";
 import { Inter } from "next/font/google";
 import dynamic from "next/dynamic";
 import Footer from "./components/footer";
-import Navbar from "./components/navbar";
+//import Navbar from "./components/navbar";
 import Providers from "./providers"; // Import the new Providers component
 import "./css/card.scss";
 import "./css/globals.scss";
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Providers> {/* Wrap your layout's content with the Providers component */}
           <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
-            <Navbar />
+            {/* <Navbar /> */} {/* <-- COMMENT THIS OUT */}
             {children}
             <ScrollToTop />
           </main>
