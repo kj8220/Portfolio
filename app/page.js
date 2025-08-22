0a8d20e1ab8fed9c267137a0158c13fc40452af9
roopanraj_perumalraj <roopanraj.perumalraj@42-q.com>
2025-08-22 15:16:02 +0530

    Feat: added about and hero section

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 // In app/page.js
 
 import { personalData } from "@/utils/data/personal-data";
-//import AboutSection from "./components/homepage/about";
+import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 //import Education from "./components/homepage/education";
 //import Experience from "./components/homepage/experience";
-//import HeroSection from "./components/homepage/hero-section";
+import HeroSection from "./components/homepage/hero-section";
 //import Projects from "./components/homepage/projects";
 // import Skills from "./components/homepage/skills"; // <-- Keep this commented out
 
@@ -22,8 +22,8 @@ export default async function Home() {
   // const data = await getData(); // If you use this, uncomment it
   return (
     <div>
-     {/* <HeroSection />*/}
-      {/*<AboutSection />*/}
+      <HeroSection />
+      <AboutSection />
       {/*<Experience />*/}
       {/* <Skills /> */} {/* <-- Keep this commented out */}
       {/* <Projects />*/}
