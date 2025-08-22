ba2dfbca1aa6882c87e3152d2f3baf2491c222a8
roopanraj_perumalraj <roopanraj.perumalraj@42-q.com>
2025-08-22 13:38:16 +0530

    commented project part

--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@ import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
-//import Experience from "./components/homepage/experience";
+import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
-import Projects from "./components/homepage/projects";
+//import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
 async function getData() {
@@ -27,9 +27,9 @@ export default async function Home() {
     <div suppressHydrationWarning >
       <HeroSection />
       <AboutSection />
-      {/*<Experience />*/}
+      <Experience />
       <Skills /> 
-      <Projects />
+      {/*<Projects />*/}
       <Education />
       <ContactSection />
     </div>
