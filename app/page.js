53e04651d4497314fd31718f572d2e024641229a
roopanraj_perumalraj <roopanraj.perumalraj@42-q.com>
2025-08-22 13:44:41 +0530

    commented contact part

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
-import ContactSection from "./components/homepage/contact";
-//import Education from "./components/homepage/education";
+//import ContactSection from "./components/homepage/contact";
+import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
@@ -30,8 +30,8 @@ export default async function Home() {
       <Experience />
       <Skills /> 
       <Projects />
-      {/*<Education />*/}
-      <ContactSection />
+      <Education />
+      {/*<ContactSection />*/}
     </div>
   )
 };
