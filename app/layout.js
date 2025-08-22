196c987b8902cb122ae62e5291ce6ccd1675894c
roopanraj_perumalraj <roopanraj.perumalraj@42-q.com>
2025-08-22 14:10:41 +0530

    adding minimal version

--- app/layout.js
+++ app/layout.js
@@ -1,34 +1,17 @@
-"use client";
+// In app/layout.js
 
-import { GoogleTagManager } from "@next/third-parties/google";
-import { Inter } from "next/font/google";
-import dynamic from "next/dynamic";
-//import Footer from "./components/footer";
-//import Navbar from "./components/navbar";
-import Providers from "./providers"; // Import the new Providers component
-import "./css/card.scss";
 import "./css/globals.scss";
 
-const inter = Inter({ subsets: ["latin"] });
-
-const ScrollToTop = dynamic(
-  () => import("./components/helper/scroll-to-top"),
-  { ssr: false }
-);
+export const metadata = {
+  title: "Portfolio of Roopan Raj",
+  description: "This is the portfolio of Roopan Raj.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Providers> {/* Wrap your layout's content with the Providers component */}
-          <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
-            {/* <Navbar /> */} {/* <-- COMMENT THIS OUT */}
-            {children}
-            <ScrollToTop />
-          </main>
-          {/*<Footer />*/}
-        </Providers>
-        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
+      <body>
+        {children}
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,43 +1,10 @@
-import { personalData } from "@/utils/data/personal-data";
-import AboutSection from "./components/homepage/about";
-import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
-import HeroSection from "./components/homepage/hero-section";
-import Projects from "./components/homepage/projects";
-import Skills from "./components/homepage/skills";
+// In app/page.js
 
-export const metadata = {
-  title: "Portfolio of Roopan Raj - Software Developer",
-  description:
-    "This is the portfolio of Roopan Raj. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
-};
-
-async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  const data = await res.json();
-
-  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
-  return filtered;
-};
-
-export default async function Home() {
-
+export default function Home() {
   return (
-    <div suppressHydrationWarning >
-      <HeroSection />
-      <AboutSection />
-      <Experience />
-      <Skills /> 
-      <Projects />
-      <Education />
-      <ContactSection />
+    <div>
+      <h1>Build Test</h1>
+      <p>This is a test to get a successful deployment.</p>
     </div>
-  )
-};
+  );
+}
