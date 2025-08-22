a1c9c231a671be0f85c1cb5efed53bb37e1889b4
roopanraj_perumalraj <roopanraj.perumalraj@42-q.com>
2025-08-22 13:35:49 +0530

    commented experience part

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 import { personalData } from "@/utils/data/personal-data";
-//import AboutSection from "./components/homepage/about";
+import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
+//import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
@@ -26,8 +26,8 @@ export default async function Home() {
   return (
     <div suppressHydrationWarning >
       <HeroSection />
-      {/*<AboutSection />*/}
-      <Experience />
+      <AboutSection />
+      {/*<Experience />*/}
       <Skills /> 
       <Projects />
       <Education />
